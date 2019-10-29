7b91f54bb687760e8079cea4cf91a00d46ab8f1f
ylcyf <ylcyf@163.com>
2019-10-29 20:52:41 +0800

    修改了一些错误：   1.将数据库连接修改为本地连接   2.商家后台安全框架有一处注解标注错误，导致此模块无法正常启动   3.sellerService的findOne方法发送参数名与后端Controller不同，修改为相同。 写完了商家信息修改功能

--- youlexuan-parent/youlexuan-shop-web/src/main/webapp/js/controller/sellerController.js
+++ youlexuan-parent/youlexuan-shop-web/src/main/webapp/js/controller/sellerController.js
@@ -1,7 +1,7 @@
  //控制层 
-app.controller('sellerController' ,function($scope,$controller   ,sellerService){	
-	
-	$controller('baseController',{$scope:$scope});//继承
+app.controller('sellerController' ,function($scope, $controller, sellerService, loginService){
+
+    $controller('baseController',{$scope:$scope});//继承
 	
     //读取列表数据绑定到表单中  
 	$scope.findAll=function(){
@@ -21,27 +21,24 @@ app.controller('sellerController' ,function($scope,$controller   ,sellerService)
 			}			
 		);
 	}
-	
+
 	//查询实体 
-	$scope.findOne=function(id){
-		alert(1231231);
-		alert(id);
-		sellerService.findOne(id).success(
-			function(response){
-				$scope.entity= response;					
-			}
-		);				
+	$scope.findOne=function(){
+		loginService.loginName().success(
+			function (response) {
+                sellerService.findOne(response.loginName).success(
+                    function(response){
+                        $scope.entity= response;
+                    }
+                );
+            }
+		);
+
 	}
 	
 	//保存 
-	$scope.save=function(){				
-		var serviceObject;//服务层对象  				
-		if($scope.entity.id!=null){//如果有ID
-			serviceObject=sellerService.update( $scope.entity ); //修改  
-		}else{
-			serviceObject=sellerService.add( $scope.entity  );//增加 
-		}				
-		serviceObject.success(
+	$scope.save=function(){
+		sellerService.update($scope.entity).success(
 			function(response){
 				if(response.success){
 					//重新查询 
@@ -90,9 +87,5 @@ app.controller('sellerController' ,function($scope,$controller   ,sellerService)
                 }
             }
         );
-    }
-
-    $scope.abc = function () {
-		alert(324232);
     }
 });	
--- youlexuan-parent/youlexuan-shop-web/src/main/webapp/js/service/sellerService.js
+++ youlexuan-parent/youlexuan-shop-web/src/main/webapp/js/service/sellerService.js
@@ -11,7 +11,7 @@ app.service('sellerService',function($http){
 	}
 	//查询实体
 	this.findOne=function(id){
-		return $http.get('../seller/findOne.do?id='+id);
+		return $http.get('../seller/findOne.do?sellerId='+id);
 	}
 	//增加 
 	this.add=function(entity){
